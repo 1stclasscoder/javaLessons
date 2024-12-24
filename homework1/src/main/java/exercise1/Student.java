5e399ae242c76d62032b5773360e9b4432ac27b4
German Muraev <german_muraev@mail.ru>
2024-12-24 16:35:17 +0300

    FP

--- homework1/src/main/java/exercise1/Student.java
+++ homework1/src/main/java/exercise1/Student.java
@@ -48,7 +48,7 @@ public class Student {
         return "Student{" +
                 "name='" + name + '\'' +
                 ", age=" + age +
-                ", avgAge=" + avgPoint +
+                ", avgPoint=" + avgPoint +
                 '}';
     }
 
